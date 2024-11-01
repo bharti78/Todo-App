2e7539b076231f650523338c0f63ee528f337ec5
bharti78 <2023kucp1049@iiitkota.ac.in>
2024-11-01 21:24:35 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import "./App.css";
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    setTodoItems((currValue) => [
+      ...currValue,
+      { name: itemName, dueDate: itemDueDate },
+    ]);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo onNewItem={handleNewItem} />
+      {todoItems.length === 0 && (
+        <WelcomeMessage todoItems={todoItems}></WelcomeMessage>
+      )}
+      <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItem} />
+    </center>
+  );
+}
+
+export default App;
